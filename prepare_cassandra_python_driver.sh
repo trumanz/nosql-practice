f76978cbcbe9984e49ac65378975516fd7eb3331
Truman Zhou <truman.ck.zhou@gmail.com>
2016-01-08 18:19:16 +0800

    devl

--- prepare_cassandra_python_driver.sh
+++ prepare_cassandra_python_driver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get update  \
+sudo apt-get update   \
 && sudo apt-get install -y python-dev \
 && sudo pip install cassandra-driver
--- src/test.py
+++ src/test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+import os
 from cassandra.cluster import Cluster
 
 
+CADDR = os.environ['CASSANDRA_ADDRESS']
+cluster = Cluster([CADDR])
+session = cluster.connect()
 
-cluster = Cluster()
-session = cluster.connect('demo')
+session.execute("CREATE KEYSPACE IF NOT EXISTS Excelsior  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 }")
+#print session.execute("DESCRIBE KEYSPACES")
